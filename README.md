a3f83d30cbf96167f8aaa3da5c1b674e86842e04
Wighawag <wighawag@gmail.com>
2014-09-19 21:01:58 +0100

    working using include and cl command

---
+++ README.md
@@ -0,0 +1,2 @@
+
+cl ..\src\haxetest.cpp ..\..\TestApp\Main.lib I\HaxeToolkit\haxe\lib\hxcpp\3,1,39\include /I..\..\TestApp\include
--- cpp/src/haxetest.cpp
+++ cpp/src/haxetest.cpp
@@ -1,18 +1,12 @@
 #include <iostream>
 
-
-class App // Declared here for extern reasons, but implemented in haxe
-{
-   public:
-     static void update();
-     static void init();
-};
+#include "App.h"
 
 // Glue imeplementation cpp side
 struct HaxeApp //: public VirtualAppBase
 {
-   void init() { ::App::init(); }
-   void update() { ::App::update(); }
+   void init() { ::App_obj::init(); }
+   void update() { ::App_obj::update(); }
 };
 
 
