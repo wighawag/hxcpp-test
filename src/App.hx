c5b22ad66c2b70c205dd1066689c33e46062a28f
ronan <ronan>
2014-11-26 14:15:52 +0000

    clean up

--- src/App.hx
+++ src/App.hx
@@ -10,10 +10,8 @@ class App
    public static var instance:AppInstance; // Gets set from your "main" routine
    public static function init(value : Int) : Int{ instance.init(); return 0; }
    public static function update(rect : Pointer<Rectangle>) : Int{ 
-   	var actualRect = rect.value;
-   	actualRect.set_values(4,200);
-   	//trace(actualRect.area());
-   	instance.update(actualRect.area()); 
+   	rect.ref.set_values(4,200);
+   	instance.update(rect.ref.area()); 
 
    	return 0;
    }
--- src/engine/Rectangle.hx
+++ src/engine/Rectangle.hx
@@ -1,11 +1,9 @@
 package engine;
 
-
 @:structAccess
 @:unreflective
 @:include("Rectangle.h")
 @:native("Rectangle")
-@:keep
 extern class Rectangle{
     public function set_values(w : Int, h : Int) : Void;
     public function area() : Int;
